a9dee3e34b289c156bebfe28a589ee137f4823a2
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-10-20 18:43:02 +0200

    Initial commit

---
+++ src/main/java/com/example/zadanie31/Zadanie31Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie31Application.class, args);
+    }
+
+}
